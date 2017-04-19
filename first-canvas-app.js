bdf97dbe057abe8b9b0c647825e9f44c42d09a92
Francyles <francyles06@gmail.com>
2017-04-18 21:07:56 -0300

    refatoracao, pick de reta, area, dragdrop de ponto

--- first-canvas-app.js
+++ first-canvas-app.js
@@ -1,14 +1,18 @@
 window.onload = function(){
-	initCanvas();
 	document.getElementById("load-canvas").addEventListener('change', loadTextFile, false);
+	createCanvasAux();
 }
 
-var canvas_obj = {
-	'points' : [],
-	'lines' : [],
-	'polylines' : [],
-	'polygons' : []
-};
+var canvas_obj = initCanvas();
+
+function initCanvas(){
+	return {
+		'points' : [],
+		'lines' : [],
+		'polylines' : [],
+		'polygons' : []
+	};
+}
 
 function saveTextFile() {
 	var text = JSON.stringify(canvas_obj);
@@ -33,28 +37,37 @@ function loadTextFile(evt){
 
 }
 
-
-function initCanvas(){
-	getCanvas();
+function createCanvasAux(){
+	var canvas_aux = document.createElement('canvas');
+	var container = getCanvas().parentNode;
+	canvas_aux.id = 'canvas-aux';
+	canvas_aux.width = getCanvas().width;
+	canvas_aux.height = getCanvas().height;
+	container.appendChild(canvas_aux);
 }
 
 function getCanvas(){
 	return document.getElementById("canvas-1");
 }
 
+function getCanvasAux(){
+	return document.getElementById("canvas-aux");
+}
+
 function reloadCanvas(){
 	document.getElementById("painel").innerHTML = "<canvas id='canvas-1' width='800px' height='500px'></canvas>";
+	createCanvasAux();
 	
-	function printPoints(canvas, point){
+	this.printPoints = function(canvas, point){
 		var context = canvas.getContext('2d');    
 	    context.fillRect(point.x,point.y,2,2);
 	}
 
-	function printLine(canvas, line){
+	this.printLine = function(canvas, line){
 		var context = canvas.getContext('2d');
 		context.beginPath();
-		context.moveTo(line['p1'].x, line['p1'].y);
-		context.lineTo(line['p2'].x, line['p2'].y);
+		context.moveTo(line[0].x, line[0].y);
+		context.lineTo(line[1].x, line[1].y);
 		context.stroke();
 	}
 
@@ -88,6 +101,7 @@ function eraseCanvas(){
 	var canvas = getCanvas();
 	var context = canvas.getContext("2d");
 	context.clearRect(0, 0, canvas.width, canvas.height);
+	canvas_obj = initCanvas();
 }
 
 function isPoint(target, myPoint){
@@ -107,7 +121,7 @@ function isPoint(target, myPoint){
 
 function drawPoint(){
 	reloadCanvas();
-	var canvas = getCanvas();
+	var canvas = getCanvasAux();
 	function pointing(canvas, x, y) {
 	    var context = canvas.getContext('2d');    
 	    context.fillRect(x,y,2,2);
@@ -125,45 +139,57 @@ function drawPoint(){
 
 function drawLine(){
 	reloadCanvas();
-	var canvas = getCanvas();
+	var canvas = getCanvasAux();
 	var points = [];
 	var count = 0;
+	var init = false;
+	var context = canvas.getContext('2d');
 
-	function savePoint(point, count){
-		if(count<=2)
-			points.push(point);
+	function savePoint(point){
+		points.push(point);
 	}
 
-	function printLine(canvas, points){
-		var context = canvas.getContext('2d');
+	function initLine(evt){
+    	var mousePos = getMousePos(canvas, evt);
+    	savePoint(mousePos);
+    	init = true;
+    	console.log(points[0]);
+
+	}
+	function printLine(evt){
+		if(!init) return;	
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		var mousePos = getMousePos(canvas, evt);
 		context.beginPath();
 		context.moveTo(points[0].x, points[0].y);
-		context.lineTo(points[1].x, points[1].y);
+		context.lineTo(mousePos.x, mousePos.y);
 		context.stroke();
-		var line = {'p1': points[0] , 'p2': points[1] };
+	}
+	function saveLine(evt){
+		var mousePos = getMousePos(canvas, evt);
+		points.push({'x': mousePos.x, 'y':mousePos.y});
+		var line = [{'x': points[0].x, 'y':points[0].y},
+					{'x': points[1].x, 'y':points[1].y} 
+					];
 		canvas_obj['lines'].push(line);
+		init = false;
+		points = [];
+		reloadCanvas();
 	}
+	canvas.addEventListener('mousedown', initLine, false);
+	canvas.addEventListener('mousemove', printLine, false);
+	canvas.addEventListener('mouseup', saveLine, false);
 
-	function mainLine(evt){
-    	var mousePos = getMousePos(canvas, evt);
-    	savePoint(mousePos, count);
-    	count+=1;
-    	console.log(points);
-    	if(count >=2 ){
-    		printLine(canvas, points);
-    		count = 0;
-    		points = [];
-    	}
-	}
-
-	canvas.addEventListener('mousedown', mainLine, false);
-	canvas.removeEventListener('mousedown', mainLine, true);
+	 // canvas.removeEventListener('mousedown', initLine, true);
+	 // canvas.removeEventListener('mousemove', printLine, true);
+	 // canvas.removeEventListener('mouseup', saveLine, true);
+	//canvas.removeEventListener('mousedown', mainLine, true);
 	
 }
 
 function drawPolyline(){
 	reloadCanvas();
-	var canvas = getCanvas();
+	var canvas = getCanvasAux();
 	var points = [];
 	var lines = [];
 	var polylines = [];
@@ -174,7 +200,9 @@ function drawPolyline(){
 	}
 
 	function saveLines(points){
-		var line = {'p1' : points[0], 'p2': points[1]};
+		var line = [{'x': points[0].x, 'y':points[0].y},
+					{'x': points[1].x, 'y':points[1].y} 
+					];
 		lines.push(line);
 		console.log(lines);
 	}
@@ -184,8 +212,8 @@ function drawPolyline(){
 		for (var i = 0; i < lines.length; i++){
 			polyline.push(lines[i]);
 		}
-		//polylines.push(polyline);
 		canvas_obj['polylines'].push(polyline);
+		reloadCanvas();
 	}
 
 	function printLine(canvas, points){
@@ -220,7 +248,7 @@ function drawPolyline(){
 
 function drawPolygon(){
 	reloadCanvas();
-	var canvas = getCanvas();
+	var canvas = getCanvasAux();
 	var points = [];
 	var lines = [];
 	var polygons = [];
@@ -231,7 +259,9 @@ function drawPolygon(){
 	}
 
 	function saveLines(points){
-		var line = {'p1' : points[0], 'p2': points[1]};
+		var line = [{'x': points[0].x, 'y':points[0].y},
+					{'x': points[1].x, 'y':points[1].y} 
+					];
 		lines.push(line);
 	}
 
@@ -241,6 +271,7 @@ function drawPolygon(){
 			polygon.push(lines[i]);
 		}
 		canvas_obj['polygons'].push(polygon);
+		reloadCanvas();
 	}
 
 
@@ -259,9 +290,9 @@ function drawPolygon(){
     	savePoint(mousePos);
     	
     	if(points.length >=2 ){
-    		if(lines.length > 0 && isPoint(lines[0]["p1"], mousePos)){
-    			console.log(isPoint(lines[0]["p1"], mousePos));
-    			printLine(canvas, [points[0], lines[0]["p1"]]);
+    		if(lines.length > 0 && isPoint(lines[0][0], mousePos)){
+    			console.log(isPoint(lines[0][0], mousePos));
+    			printLine(canvas, [points[0], lines[0][0]]);
     			savePolygons(lines);
     			points = [];
     			lines = [];
@@ -279,49 +310,187 @@ function drawPolygon(){
 }
 
 /**** Funções de seleção de objetos ****/
-function selectionObj(){
+function selectionObj(someFunction){
 	reloadCanvas();
-	var canvas = getCanvas();
+	var canvas = getCanvasAux();
 	
 	function select(evt){
 		console.log("selectionObj");
 		var mousePos = getMousePos(canvas, evt);
-		for (var i=0; i<canvas_obj['polygons'].length; i++){
-				if(pickAreaAngle(mousePos, canvas_obj['polygons'][i])){
-					console.log(canvas_obj['polygons'][i]);
+		for (var i=0; i< canvas_obj['points'].length; i++){ //verifica pontos
+			if(isPoint(canvas_obj['points'][i], mousePos)){
+				dragAndDrop(canvas_obj['points'][i]);
+			}
+		}
+		for (var i=0; i<canvas_obj['lines'].length; i++){ //verifica linhas
+				if(pickLine(canvas_obj['lines'][i], mousePos)){
+					//função de agarrar o objeto
+					dragAndDrop(canvas_obj['lines'][i], mousePos);
+					//console.log(canvas_obj['lines'][i]);
 				}
 		}
+		for (var i=0; i<canvas_obj['polylines'].length; i++){ //verifica poligonos
+				if(pickLine(canvas_obj['polylines'][i],mousePos)){
+					//função de agarrar o objeto
+					console.log(canvas_obj['polylines'][i]);
+					//dragAndDrop(canvas_obj['polylines'][i], mousePos);
+				}
+		}
+		for (var i=0; i<canvas_obj['polygons'].length; i++){ //verifica poligonos
+				if(pickAreaAngle(mousePos, canvas_obj['polygons'][i])){
+					//função de agarrar o objeto
+					dragAndDrop(canvas_obj['polygons'][i], mousePos);
+				}
+		}
+		
 	}
 	canvas.addEventListener('mousedown',select, false);
+	//canvas.removeEventListener('mousedown',select, true);
 
 }
-
+/******Pick de area pela soma dos angulos********/
 function pickAreaAngle(point, poligono){
 	var soma = 0;
 	var lines = [];
 	
 	for(var i = 0; i < poligono.length; i++){
-		var v = [poligono[i]["p1"].x - point.x , poligono[i]["p2"].y - point.y];
-		console.log(v);
+		var v = [poligono[i][0].x - point.x , poligono[i][0].y - point.y];
 		lines.push(v);
-		var context = getCanvas().getContext('2d');
-		context.beginPath();
-		context.moveTo(poligono[i]["p1"].x, poligono[i]["p2"].y);
-		context.lineTo(point.x, point.y);
-		context.stroke();
 	}
-	var i;
-	for(i = 0; i < lines.length; i++){
+
+	for(var i = 0; i < lines.length; i++){
 		var cosAng;
 		if(i == lines.length-1)
 			cosAng = (lines[i][0] * lines[0][0] + lines[i][1] * lines[0][1]) / (Math.sqrt(Math.pow(lines[i][0],2) + Math.pow(lines[i][1],2)) * Math.sqrt(Math.pow(lines[0][0],2) + Math.pow(lines[0][1],2)));
 		else
 			cosAng = (lines[i][0] * lines[i+1][0] + lines[i][1] * lines[i+1][1]) / (Math.sqrt(Math.pow(lines[i][0],2) + Math.pow(lines[i][1],2)) * Math.sqrt(Math.pow(lines[i+1][0],2) + Math.pow(lines[i+1][1],2)));
 		var ang = Math.acos(cosAng);
-		soma = soma + Math.abs(ang);
+		soma = soma + ang;
 	}
 
-	console.log(soma);
+	console.log(soma == 2*Math.PI);
 
 	return (soma == 2*Math.PI);
+}
+/***Pick de reta **/
+function pickLine(line, clickPoint){
+  
+  function code(x, y, clickX, clickY, tol, codigo){
+  	for(var i = 0; i < 4; i++){
+		codigo[i] = 0;
+	}
+	  
+	if(x < clickX - tol){codigo[0] = 1;}
+	if(x > clickX + tol){codigo[1] = 1;}
+	if(y < clickY - tol){codigo[2] = 1;}
+	if(y > clickY + tol){codigo[3] = 1;}
+  }
+
+  var code1 = [];
+  var code2 = [];
+  var j;
+  var newY;
+  var tol = 5;
+  
+  x1 = line[0].x;
+  y1 = line[0].y;
+  x2 = line[1].x;
+  y2 = line[1].y;
+  
+  ymin = clickPoint.y - tol;
+  ymax = clickPoint.y + tol;
+  xmin = clickPoint.x - tol;
+  xmax = clickPoint.x + tol;
+  
+  
+  code(x2, y2, clickPoint.x, clickPoint.y, tol, code2);
+
+  var cont = 0;
+  do{
+    code(x1, y1, clickPoint.x, clickPoint.y, tol, code1);
+    
+    for(j = 0; j < 4; j++){
+        if(code1[j] && code2[j]){
+        break;
+      }
+      }
+    if (j != 4) break;
+
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+
+    if (code1[0]) {y1 = (((xmin - x1) * dy)/dx) + y1; x1 = xmin}
+    else if (code1[1]) {y1 = (((xmax - x1) * dy)/dx) + y1; x1 = xmax}
+    else if (code1[2]) {x1 = (((ymin - y1) * dx)/dy) + x1; y1 = ymin}
+    else if (code1[3]) {x1 = (((ymax - y1) * dx)/dy) + x1; y1 = ymax}
+    else return true;
+  } while(1);
+
+  return false;
+}
+
+
+
+function dragAndDrop(obj, mp){
+	var obj = obj;
+	var context = getCanvasAux().getContext('2d');
+	var moveObj, leaveObj, diff_x, diff_y;
+	
+	function isArray(obj){
+    	return !!obj && obj.constructor === Array;
+	}
+
+	if(!isArray(obj)){
+		moveObj = function(e){
+			context.clearRect(0, 0, getCanvasAux().width, getCanvasAux().height);
+			obj.x = e.x - getCanvasAux().offsetLeft;
+			obj.y = e.y - getCanvasAux().offsetTop;
+			context.fillRect(obj.x,obj.y,2,2);	
+		}
+
+		leaveObj = function (e){
+			context.fillRect(obj.x,obj.y,2,2);
+			reloadCanvas();
+		}
+	}
+	else{
+		moveObj = function(e){
+			console.log(mp.x, mp.y);
+			context.clearRect(0, 0, getCanvasAux().width, getCanvasAux().height);
+			var mousePos = getMousePos(getCanvasAux(), e);
+			var aux_x = mousePos.x - getCanvasAux().offsetLeft;
+			var aux_y = mousePos.y - getCanvasAux().offsetTop;
+			diff_x = aux_x - mp.x;
+			diff_y =  aux_y - mp.y;
+			console.log(diff_x, diff_y);
+
+		}
+
+		leaveObj = function (e){
+			var mousePos = getMousePos(getCanvasAux(), e);
+			var aux_x = mousePos.x - getCanvasAux().offsetLeft;
+			var aux_y = mousePos.y - getCanvasAux().offsetTop;
+			diff_x = aux_x - mp.x;
+			diff_y = aux_y - mp.y;
+			for(var i=0; i<obj.length; i++){
+				obj[i][0].x += diff_x; 
+				obj[i][0].y += diff_y;
+				obj[i][1].x += diff_x; 
+				obj[i][1].y += diff_y;
+			}
+			for(var i=0; i<obj.length; i++){
+				context.beginPath();
+				context.moveTo(obj[i][0].x, obj[i][0].y);
+				context.lineTo(obj[i][1].x, obj[i][1].y);
+				context.stroke();
+			}
+
+			reloadCanvas();
+		}	
+	}
+
+	getCanvasAux().addEventListener("mousemove", moveObj, false);
+	getCanvasAux().addEventListener("mouseup", leaveObj, false);
+
+	
 }
--- untitled.js
+++ untitled.js
@@ -1,239 +1,28 @@
-canvas.addEventListener('mousedown', function(evt) {
-    	evt.stopPropagation();
-    	var mousePos = getMousePos(canvas, evt);
-    	savePoint(mousePos);
-    	    	
-    	if(points.length >=2 ){
-    		for(var i = 0; i<lines.length; i++){
-    			if(isPoint(lines[i]['p2'], points[0])){
-    				points[0] = lines[i]['p2'];
-    			}
-    		}
-			printLine(canvas, points);
-    		saveLines(points);
-    		points = [];
-    	}
-	}, false);
+var Point = function(x, y){
+    this.x = x;
+    this.y = y;
+    this.color = "black";
 
+    this.setColor = function(color){
+        this.color = color;
+    };
 
-
-
-
-
-
-
-
-
-window.onload = function(){
-    initCanvas();
-}
-
-function initCanvas(){
-    getCanvas();
-    //getMousePosition(getCanvas());
-    //drawPoint();
-    //drawLine();
-    //drawPolyline();
-    //drawPolygon();
-}
-
-function getCanvas(){
-    return document.getElementById("canvas-1");
-}
-
-function getMousePosition(canvas){
-    var position;
-
-    function getMousePos(canvas, evt) {
-        var rect = canvas.getBoundingClientRect();
-        return {
-          x: evt.clientX - rect.left,
-          y: evt.clientY - rect.top
-        };
-    }
-
-    canvas.addEventListener('mousedown', function(evt) {
-        evt.stopPropagation();
-        position = getMousePos(canvas, evt);
-    }, false);
-
-    return position;
-}
-
-function eraseCanvas(){
-    var canvas = getCanvas();
-    var context = canvas.getContext("2d");
-    context.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-
-
-function isPoint(target, myPoint){
-    var T = 10;
-
-    if (target === undefined || myPoint === undefined) return false;
-
-    if(myPoint.x  <= target.x + T && myPoint.y  <= target.y + T){
-        if( myPoint.x  >= target.x - T && myPoint.y >= target.y - T){
-            return true;
-        }
-    }
-
-    return false;
-}
-
-function drawPoint(){
-    var canvas = getCanvas();
-    function pointing(canvas, x, y) {
-        var context = canvas.getContext('2d');    
-        context.fillRect(x,y,1,1);
-    }
-
-    function main(){
-        var mousePos = getMousePosition(canvas);
-        pointing(canvas, mousePos.x, mousePos.y);
-    }
-    main();
-
-}
-
-function drawLine(){
-    var canvas = getCanvas();
-    var points = [];
-    var count = 0;
-
-    function savePoint(point, count){
-        if(count<=2)
-            points.push(point);
-    }
-
-    function printLine(canvas, points){
+    this.draw = function(canvas){
         var context = canvas.getContext('2d');
-        context.beginPath();
-        context.moveTo(points[0].x, points[0].y);
-        context.lineTo(points[1].x, points[1].y);
-        context.stroke();
-    }
+        context.fillRect(this.x,this.y,2,2);
+    };
+};
 
-    function main(){
-        var mousePos = getMousePosition(canvas);
-        savePoint(mousePos, count);
-        count+=1;
-        console.log(points);
-        if(count >=2 ){
-            printLine(canvas, points);
-            count = 0;
-            points = [];
-        }
-    }
-    main(); 
-}
-
-function drawPolyline(){
-    var canvas = getCanvas();
-    var points = [];
-    var lines = [];
-    var polylines = [];
+var Line = function(p1, p2){
+    this.p1 = p1;
+    this.p2 = p2;
+    this.color = "black";
     
-    function savePoint(point){
-        if(points.length<=2)
-            points.push(point);
-    }
+    this.setColor = function(color){
+        this.color = color;
+    };
 
-    function saveLines(points){
-        var line = {'p1' : points[0], 'p2': points[1]};
-        lines.push(line);
-    }
-
-    function savePolylines(lines){
-        var polyline = [];
-        for (var i = 0; i < lines.length; i++){
-            polyline.push(lines[i]);
-        }
-        polylines.push(polyline);
-    }
-
-    function printLine(canvas, points){
-        var context = canvas.getContext('2d');
-        context.beginPath();
-        context.moveTo(points[0].x, points[0].y);
-        context.lineTo(points[1].x, points[1].y);
-        context.stroke();
-        saveLines(points);
-    }
-    
-
-    function main(){
-        var mousePos = getMousePosition(canvas);
-        savePoint(mousePos);
-        if(points.length >=2 ){
-            for(var i = 0; i<lines.length; i++){
-                if(isPoint(lines[i]['p2'], points[0])){
-                    points[0] = lines[i]['p2'];
-                }
-            }
-            printLine(canvas, points);
-            saveLines(points);
-            points = [];
-        }
-    }
-    
-    main();
-}
-
-function drawPolygon(){
-    var canvas = getCanvas();
-    var points = [];
-    var lines = [];
-    var polygons = [];
-    
-    canvas.removeEventListener('mousedown');
-
-    function savePoint(point){
-        if(points.length<=2)
-            points.push(point);
-    }
-
-    function saveLines(points){
-        var line = {'p1' : points[0], 'p2': points[1]};
-        lines.push(line);
-    }
-
-    function savePolygons(lines){
-        var polygon = [];
-        for (var i = 0; i < lines.length; i++){
-            polygon.push(lines[i]);
-        }
-        polygons.push(polygon);
-    }
-
-    function printLine(canvas, points){
-        var context = canvas.getContext('2d');
-        context.beginPath();
-        context.moveTo(points[0].x, points[0].y);
-        context.lineTo(points[1].x, points[1].y);
-        context.stroke();
-        saveLines(points);
-    }
-
-    function main(){
-        var mousePos = getMousePosition(canvas);
-        savePoint(mousePos);
+    this.draw = function(canvas){
         
-        if(points.length >=2 ){
-            if(lines.length > 0 && isPoint(lines[0]["p1"], mousePos)){
-                console.log(isPoint(lines[0]["p1"], mousePos));
-                printLine(canvas, [points[0], lines[0]["p1"]]);
-                savePolygons(lines);
-                points = [];
-                lines = [];
-            }
-            else{
-                printLine(canvas, points);
-                points.shift(); 
-            }
-        }
-    }
-
-    main();
+    }    
 }
